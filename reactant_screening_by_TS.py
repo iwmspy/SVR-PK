8cf9bb43f0b87e74548320b3a9694ea09f923c64
iwmspy <iwasaki.yuto.jz4@ms.naist.jp>
2025-05-14 12:57:42 +0900

    Add TS

--- reactant_screening_by_TS.py
+++ reactant_screening_by_TS.py
@@ -120,7 +120,7 @@ def main():
             json.dump(ts_dict,f)
 
         with timer() as t:
-            run('_benchmarking.Thompson.ts_main',
+            run('_benchmarking.Thompson.thompson',
                 f'{rs.dir_cand}/ts_envs.json')
             rs.res_dict['sampling_time'] = t.get_runtime()
         
